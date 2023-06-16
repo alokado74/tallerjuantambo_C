a1c7436cde32502038d31fdb361e7ec3ecf9bd30
alokado74 <136029534+alokado74@users.noreply.github.com>
2023-06-16 01:38:54 +0000

    importacion de archivos

---
+++ programa.py
@@ -0,0 +1,8 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from geometria import distancia
+
+print("Suma: ",sumar(2, 3))
+print("Resta: ",restar(4, 2))
+print("Multiplicacion: ",multiplicar(5, 2))
+print("Divicion: ",dividir(10, 2))
+print("Distancia: ",distancia(1,2,4,5))
